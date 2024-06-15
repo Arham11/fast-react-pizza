14cc13f0fed6309bc5aeb679c464ad98d73870f8
Arham Chowdhry <arham.chowdhry@abbott.com>
2024-06-15 16:57:26 +0530

    add tailwind config files and load external fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    // direct property (like fontfamily below) will override all the fontfamily of  tailwind
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    // use to inject additional custom classes
+    extend: {
+      height: {
+        screen: "100dvh", // dynamic verticle height (better the vh in mobile screen)
+      },
+    },
   },
   plugins: [],
 };
