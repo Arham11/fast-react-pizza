b290b47f1f88d85760068f05208792001d43041e
Arham Chowdhry <arham.chowdhry@abbott.com>
2024-06-16 05:07:45 +0530

    useSelector hook to call the username from redux-store

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -8,6 +8,7 @@ const isValidPhone = (str) =>
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 // import { createOrder } from '../../services/apiRestaurant';
 
 const fakeCart = [
@@ -36,6 +37,8 @@ const fakeCart = [
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
+  const username = useSelector((state) => state.user.username);
+
   const cart = fakeCart;
   const formErrors = useActionData();
   const navigation = useNavigation();
@@ -50,7 +53,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            defaultValue={username}
+            name="customer"
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
