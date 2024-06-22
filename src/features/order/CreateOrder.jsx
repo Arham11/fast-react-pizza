71231d3651c1de33c8e3c4042ae57447b9e9f747
Arham Chowdhry <arham.chowdhry@abbott.com>
2024-06-22 17:38:46 +0530

    thunks using redux toolkit with createAsyncThunks

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -6,7 +6,7 @@ const isValidPhone = (str) =>
   );
 
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 
 import store from "../../store";
@@ -15,6 +15,7 @@ import Button from "../../ui/Button";
 import { formatCurrency } from "../../helpers/helpers";
 import { createOrder } from "../../services/apiRestaurant";
 import EmptyCart from "../cart/EmptyCart";
+import { fetchAddress } from "../user/userSlice";
 // import { createOrder } from '../../services/apiRestaurant';
 
 // const fakeCart = [
@@ -43,7 +44,14 @@ import EmptyCart from "../cart/EmptyCart";
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === "loading";
 
   var cart = useSelector(getCart);
   const formErrors = useActionData();
@@ -52,13 +60,13 @@ function CreateOrder() {
   const totalCartPrice = useSelector(getTotalPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
+  const dispatch = useDispatch();
 
   if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h2>
-
       {/* <Form method="POST" action="order/new"> */}
       {/* the action is not mandatory to add by default react know about the action */}
       <Form method="POST">
@@ -85,16 +93,37 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+            {errorAddress && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className="absolute bottom-[3px] right-[3px] top-[3px] z-50 md:right-[6px] md:top-[6px]">
+              <Button
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+                disabled={isSubmitting || isLoadingAddress}
+              >
+                get address
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -113,6 +142,16 @@ function CreateOrder() {
         </div>
 
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+        <input
+          type="hidden"
+          name="position"
+          value={
+            position.longitude && position.longitude
+              ? `${position.latitude}, ${position.longitude}`
+              : ""
+          }
+        />
+
         <div>
           <Button disabled={isSubmitting} type="primary">
             {isSubmitting
